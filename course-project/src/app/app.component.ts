d5cf7bfa08a8162ccf99d6cd655bb6609316021a
Xsorter Nelin <xsorter@gmail.com>
2018-10-21 17:54:06 +0300

    routing with ngif added

--- course-project/src/app/app.component.ts
+++ course-project/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string){
+    this.loadedFeature = feature;
+    console.log('loaded from app component = ', this.loadedFeature)
+  }
 }
--- course-project/src/app/header/header.component.ts
+++ course-project/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
@@ -8,5 +9,10 @@ import { Component } from '@angular/core';
   
 
 export class HeaderComponent {
+  @Output() featureSelected = new EventEmitter<string>();
 
+  onSelect(feature: string){
+    this.featureSelected.emit(feature);
+    console.log('header', feature)
+  }
 }
