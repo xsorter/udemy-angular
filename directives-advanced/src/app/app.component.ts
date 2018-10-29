a03084e9d4ec87e9869b81a2d04f8dcbe2e59e3c
Xsorter Nelin <xsorter@gmail.com>
2018-10-29 23:20:47 +0200

    advanced directives lesson

---
+++ directives-advanced/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  numbers = [1, 2, 3, 4, 5];
+  onlyOdd = false;
+  onlyOddNumbers = [1, 3, 5];
+  onlyEvenNumbers = [2, 4];
+}
