e9dea46175464b7083c8138011eee240e93018e6
Xsorter Nelin <xsorter@gmail.com>
2018-10-31 00:28:58 +0200

    adding HostBinding to attribute directive

--- directives-advanced/src/app/better-highlight/better-highlight.directive.ts
+++ directives-advanced/src/app/better-highlight/better-highlight.directive.ts
@@ -1,14 +1,25 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(){
-    this.renderer.setStyle(this.elRef.nativeElement, 'background', 'blue');
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', '#fff')
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', '#d2d2d2')
+  }
+
+  @HostListener('mouseover') mouseover() {
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background', 'blue');
+    this.backgroundColor = 'blue';
+  }
+
+  @HostListener('mouseleave') mouseleave() {
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background', 'transparent');
+    this.backgroundColor = 'transparent';
   }
 }
