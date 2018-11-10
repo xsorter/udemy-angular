3ba75160a5bccf1862efd315f23d84937cec3910
Xsorter Nelin <xsorter@gmail.com>
2018-11-10 20:00:35 +0200

    add data service

---
+++ services-start/src/app/accounts.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+
+
+@Injectable()
+export class AccountsService{
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'active'
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive'
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown'
+    }
+  ];
+
+  addAccount(name: string, status: string){
+    this.accounts.push({name: name, status: status})
+  }
+
+  updateAccount(id: number, status: string){
+    this.accounts[id].status = status;
+  }
+}
--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -1,22 +1,19 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  providers: [LoggingService, AccountsService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
-  constructor(private loggingService: LoggingService){}
+  constructor(private loggingService: LoggingService, private accountService: AccountsService){}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    this.accountService.addAccount(accountName, accountStatus);
     this.loggingService.logStatusChange(accountStatus)
   }
 }
