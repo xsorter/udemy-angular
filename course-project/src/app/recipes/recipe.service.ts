ea873fe4aea338693fc15fcdf7b39989fe5d3715
Xsorter Nelin <xsorter@gmail.com>
2018-11-14 23:28:43 +0200

    passing ingredients from recipe to shopping list

--- course-project/src/app/recipes/recipe.service.ts
+++ course-project/src/app/recipes/recipe.service.ts
@@ -1,23 +1,40 @@
 import { Recipe } from "./recipe.model";
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
-
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
-      'Pizza recipe', 
-      'this is test recipe', 
-      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'
+      'Authentic German Schnitzel', 
+      'Best Schnitzel!', 
+      'https://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-8-edited-6.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Potato', 2)
+      ]
       ),
     new Recipe(
-      'Pizza recipe 2', 
-      'this is test recipe 2', 
-      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'
+      'Burger', 
+      'classic Burger', 
+      'https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.myrecipes.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fclassic-burgers-u.jpg%3Fitok%3DNwtAcM-7&w=800&c=sc&poi=face&q=85',
+      [
+        new Ingredient('Beef', 1),
+        new Ingredient('Onion', 2)
+      ]
       ),
   ];
+
+  constructor(private shoppingService: ShoppingListService){}
 
   getRecipes(){
     return this.recipes.slice();
   }
+
+  addIngredientsToList(ingredients: Ingredient[]){
+    this.shoppingService.addIngredients(ingredients)
+  }
+
 }
