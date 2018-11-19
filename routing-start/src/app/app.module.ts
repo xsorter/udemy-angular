9f1ab3517842c52892cf2c4df6d8edf421939403
Xsorter Nelin <xsorter@gmail.com>
2018-11-20 00:46:52 +0200

    add route parameter

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { from } from 'rxjs';
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'users', component: UsersComponent},
+  {path: 'users/:id', component: UserComponent},
   {path: 'servers', component: ServersComponent},
 ]
 
