03090a5607e3b2c1f9d8d5f9735af806918fd47e
Xsorter Nelin <xsorter@gmail.com>
2018-11-21 00:21:16 +0200

    fetching route parameters

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -17,7 +17,7 @@ import { from } from 'rxjs';
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'users', component: UsersComponent},
-  {path: 'users/:id', component: UserComponent},
+  {path: 'users/:id/:name', component: UserComponent},
   {path: 'servers', component: ServersComponent},
 ]
 
