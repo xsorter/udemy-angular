f89f76606864e80eac6b62c77f5a13f10fee08ec
Xsorter Nelin <xsorter@gmail.com>
2018-11-22 00:50:31 +0200

    fetching route params reactively

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -16,6 +16,12 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     }
+    this.route.params.subscribe(
+      (params)=>{
+        this.user.id = params['id'];
+        this.user.name = params['name'];
+      }
+    )
   }
 
 }
