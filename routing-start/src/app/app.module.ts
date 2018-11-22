c02422b23d3c8738c7bdc672273e8df14bdee7bb
Xsorter Nelin <xsorter@gmail.com>
2018-11-23 00:35:33 +0200

    passing parameters and fragments

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -19,6 +19,8 @@ const appRoutes: Routes = [
   {path: 'users', component: UsersComponent},
   {path: 'users/:id/:name', component: UserComponent},
   {path: 'servers', component: ServersComponent},
+  {path: 'servers/:id', component: ServerComponent},
+  {path: 'servers/:id/edit', component: EditServerComponent},
 ]
 
 @NgModule({
