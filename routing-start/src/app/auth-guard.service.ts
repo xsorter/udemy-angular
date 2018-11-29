d4da98bee16cec9257a8bab42610a35c11210650
Xsorter Nelin <xsorter@gmail.com>
2018-11-29 23:45:43 +0200

    added auth-guard service

---
+++ routing-start/src/app/auth-guard.service.ts
@@ -0,0 +1,10 @@
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
+import { Observable } from "rxjs";
+
+export class AuthGuard implements CanActivate {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot)
+  : Observable<boolean> | Promise<boolean> | boolean
+  {
+
+  }
+}
