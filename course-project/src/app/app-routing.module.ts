8d4f69bbd759be4c45ce33e2836bd3a84fe95cd2
Xsorter Nelin <xsorter@gmail.com>
2018-12-05 23:53:36 +0200

    route paths added

--- course-project/src/app/app-routing.module.ts
+++ course-project/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from "@angular/core";
-import { Routes } from "@angular/router";
+import { Routes, RouterModule } from "@angular/router";
+import { RecipesComponent } from "./recipes/recipes.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const appRoutes: Routes = [
-
+  {path: '', redirectTo: '/recipes'},
+  {path: 'recipes', component: RecipesComponent},
+  {path: 'shopping-list', component: ShoppingListComponent}
 ]
 
 @NgModule({
-
+  imports: [RouterModule.forRoot(appRoutes)]
 })
 export class AppRoutingModule {
 
