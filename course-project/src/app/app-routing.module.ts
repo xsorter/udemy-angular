46f5f158f98b86c712ca86c7fe5581fef5ab656d
Xsorter Nelin <xsorter@gmail.com>
2018-12-06 00:12:15 +0200

    navigation added

--- course-project/src/app/app-routing.module.ts
+++ course-project/src/app/app-routing.module.ts
@@ -4,14 +4,14 @@ import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 
 const appRoutes: Routes = [
-  {path: '', redirectTo: '/recipes'},
+  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
   {path: 'recipes', component: RecipesComponent},
   {path: 'shopping-list', component: ShoppingListComponent}
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)]
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {
 
-}
+export class AppRoutingModule {}
--- course-project/src/app/header/header.component.ts
+++ course-project/src/app/header/header.component.ts
@@ -1,18 +1,11 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
   
-
 export class HeaderComponent {
-  @Output() featureSelected = new EventEmitter<string>();
-
-  onSelect(feature: string){
-    this.featureSelected.emit(feature);
-    console.log('header', feature)
-  }
+  
 }
