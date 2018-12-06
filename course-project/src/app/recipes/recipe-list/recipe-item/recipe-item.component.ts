7398fec07dfda741954257f762fa3fbab58515c1
Xsorter Nelin <xsorter@gmail.com>
2018-12-07 00:24:38 +0200

    passing parameters to the link

--- course-project/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
+
 
 @Component({
   selector: 'app-recipe-item',
@@ -9,14 +9,8 @@ import { RecipeService } from '../../recipe.service';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
-  
-  
-  constructor(private resipeService: RecipeService) { }
+  @Input() index: number;
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  onSelected(){
-    this.resipeService.recipeSelected.emit(this.recipe);
-  }
 }
