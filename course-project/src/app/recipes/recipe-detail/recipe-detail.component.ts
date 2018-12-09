d8d3918ef3ffcb8982257dff44f40b194a83fd90
Xsorter Nelin <xsorter@gmail.com>
2018-12-09 21:49:14 +0200

    navigation to edit page added

--- course-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ course-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -12,7 +12,9 @@ export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
 
-  constructor(private recipeService: RecipeService, private route: ActivatedRoute) {}
+  constructor(
+    private recipeService: RecipeService, 
+    private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
     this.route.params
@@ -26,5 +28,10 @@ export class RecipeDetailComponent implements OnInit {
 
   onAddToShoppingList(ingredient) {
     this.recipeService.addIngredientsToList(this.recipe.ingredients)
+  }
+
+  onEditRecipe(){
+    //this.router.navigate(['edit'], {relativeTo: this.route});
+    this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route})
   }
 }
