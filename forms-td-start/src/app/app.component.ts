587a073424dd0944ec8ec03e0750de96a5ed3987
Xsorter Nelin <xsorter@gmail.com>
2018-12-21 00:12:35 +0200

    handling radio buttons

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   defaultQuestion = 'pet';
+  genders = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
