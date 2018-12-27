771e9f52b2a264e59f74f16c9ba66bd406b9970b
Xsorter Nelin <xsorter@gmail.com>
2018-12-27 20:53:01 +0200

    ngModel fixed

--- form-td-practice/src/app/app.component.ts
+++ form-td-practice/src/app/app.component.ts
@@ -8,8 +8,8 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('formRef') form: NgForm;
+  dropdown = ['basic', 'advanced', 'pro'];
 
-  title = 'form-td-practice';
   onSubmit(){
     console.log(this.form);
   }
