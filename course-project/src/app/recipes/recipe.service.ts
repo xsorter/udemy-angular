20a83261ba484f3fedca117cd120dcb78e6a5055
Xsorter Nelin <xsorter@gmail.com>
2019-01-13 14:51:14 +0200

    submitting recipe edit form

--- course-project/src/app/recipes/recipe.service.ts
+++ course-project/src/app/recipes/recipe.service.ts
@@ -2,9 +2,11 @@ import { Recipe } from "./recipe.model";
 import { Injectable } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Subject } from "rxjs";
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
   
   recipes: Recipe[] = [
     new Recipe(
@@ -39,6 +41,16 @@ export class RecipeService {
 
   addIngredientsToList(ingredients: Ingredient[]){
     this.shoppingService.addIngredients(ingredients)
+  }
+
+  addRecipe(recipe: Recipe){
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe){
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
   }
 
 }
