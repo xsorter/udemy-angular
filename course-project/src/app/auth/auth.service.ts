4b801093696c670155bb949d739e4b291a8e29b9
Oleksii Nelin <xsorter@gmail.com>
2019-02-10 17:34:09 +0200

    auth guards added

--- course-project/src/app/auth/auth.service.ts
+++ course-project/src/app/auth/auth.service.ts
@@ -1,7 +1,13 @@
 import * as firebase from 'firebase';
 import 'firebase/auth';
+import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class AuthService{
+
+  constructor(private router: Router) {}
+
   token: string;
   signUp(email: string, password: string){
     firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -14,6 +20,7 @@ export class AuthService{
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(
         response => { 
+          this.router.navigate(['/']);
           firebase.auth().currentUser.getIdToken()
             .then(
               (token) => { this.token = token }
