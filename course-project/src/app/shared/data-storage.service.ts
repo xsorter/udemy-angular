c25ec724f59ea81a32e3964d463576bca0175d79
Oleksii Nelin <xsorter@gmail.com>
2019-02-23 18:39:41 +0200

    modifying requests

--- course-project/src/app/shared/data-storage.service.ts
+++ course-project/src/app/shared/data-storage.service.ts
@@ -10,24 +10,16 @@ export class DataStorageService {
   constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService){}
   
   storeRecipes(){
-    const token = this.authService.getToken();
-    /* return this.http.put('https://ng-recipe-book-f3d23.firebaseio.com/recipes.json', 
-      this.recipeService.getRecipes(), {
-        observe: 'body',
-        params: new HttpParams().set('auth', token)
-      }) */
-
     const req = new HttpRequest(
       'PUT', 
       'https://ng-recipe-book-f3d23.firebaseio.com/recipes.json', 
-      this.recipeService.getRecipes(), {reportProgress: true, params: new HttpParams().set('auth', token)}
+      this.recipeService.getRecipes(), {reportProgress: true, /*params: new HttpParams().set('auth', token)*/}
     )  
     return this.http.request(req)
   }
 
   getRecipes(){
-    const token = this.authService.getToken();
-    return this.http.get<Recipe[]>('https://ng-recipe-book-f3d23.firebaseio.com/recipes.json?auth=' + token, {
+    return this.http.get<Recipe[]>('https://ng-recipe-book-f3d23.firebaseio.com/recipes.json', {
       observe: 'body',
       responseType: 'json'
     })
