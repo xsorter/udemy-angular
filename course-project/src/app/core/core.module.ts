aae1795433484e435cf62dc95185b893081d6725
Oleksii Nelin <xsorter@gmail.com>
2019-02-23 18:11:14 +0200

    Interceptor added

--- course-project/src/app/core/core.module.ts
+++ course-project/src/app/core/core.module.ts
@@ -7,6 +7,8 @@ import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { RecipeService } from "../recipes/recipe.service";
 import { DataStorageService } from "../shared/data-storage.service";
 import { AuthService } from "../auth/auth.service";
+import { HTTP_INTERCEPTORS } from "@angular/common/http";
+import { AuthInterceptor } from "../shared/auth.interceptor";
 
 
 @NgModule({
@@ -27,6 +29,7 @@ import { AuthService } from "../auth/auth.service";
     RecipeService, 
     DataStorageService, 
     AuthService, 
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
   ]
 })
 export class CoreModule {
