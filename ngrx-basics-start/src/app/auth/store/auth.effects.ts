c4884b2147c175ad618d4a81db97660ae243d604
Oleksii Nelin <xsorter@gmail.com>
2019-03-09 20:33:46 +0200

    adding auth signup side effects

--- ngrx-basics-start/src/app/auth/store/auth.effects.ts
+++ ngrx-basics-start/src/app/auth/store/auth.effects.ts
@@ -1,14 +1,39 @@
 import { Effect, Actions, ofType } from '@ngrx/effects';
 import { Injectable } from '@angular/core';
 import * as AuthActions from '../store/auth.actions';
+import * as firebase from 'firebase';
+import { fromPromise } from 'rxjs/internal-compatibility';
 
 
 @Injectable()
 export class AuthEffects {
   @Effect()
-  authSignup = this.actions$.pipe(
-    ofType(AuthActions.TRY_SIGNUP),
+  authSignup = this.actions$
+    .ofType(AuthActions.TRY_SIGNUP)
+    .map((action: AuthActions.TrySignup) => {
+      return action.payload
+    })
+    .switchMap((authData: {username: string, password: string}) => {
+      return fromPromise(firebase.auth().createUserWithEmailAndPassword(
+        authData.username,
+        authData.password
+      ))
+    })
+    .switchMap(() => {
+      return fromPromise(firebase.auth().currentUser.getIdToken());
+    })
+    .mergeMap((token: string) => {
+      return [
+        {
+          type: AuthActions.SIGNUP
+        },
+        {
+          type: AuthActions.SET_TOKEN,
+          payload: token
+        }
+      ]
+    })
+
     
-  );
   constructor(private actions$: Actions){}
 }
