084d2c926b3fd64c2d1768685be97b34b87f9887
Oleksii Nelin <xsorter@gmail.com>
2019-03-09 19:14:05 +0200

    trySignup action added

--- ngrx-basics-start/src/app/auth/store/auth.effects.ts
+++ ngrx-basics-start/src/app/auth/store/auth.effects.ts
@@ -1,9 +1,14 @@
-import { Effect, Actions } from '@ngrx/effects';
+import { Effect, Actions, ofType } from '@ngrx/effects';
 import { Injectable } from '@angular/core';
+import * as AuthActions from '../store/auth.actions';
+
 
 @Injectable()
 export class AuthEffects {
   @Effect()
-  authSignup;
+  authSignup = this.actions$.pipe(
+    ofType(AuthActions.TRY_SIGNUP),
+    
+  );
   constructor(private actions$: Actions){}
 }
