a0f17f7ff45c48c88732afbea3f99dc25e49e8d7
Oleksii Nelin <xsorter@gmail.com>
2019-03-10 18:00:14 +0200

    auth signin effect implemented

--- ngrx-basics-start/src/app/auth/store/auth.effects.ts
+++ ngrx-basics-start/src/app/auth/store/auth.effects.ts
@@ -34,6 +34,41 @@ export class AuthEffects {
       ]
     })
 
-    
+  @Effect()
+  authSignIn = this.actions$
+    .ofType(AuthActions.TRY_SIGNIN)
+    .map(
+      (action: AuthActions.TrySignin) => {
+        return action.payload
+      }
+    )
+    .switchMap(
+      (authData: {username: string, password: string}) => {
+        return fromPromise(firebase.auth().signInWithEmailAndPassword(
+          authData.username,
+          authData.password
+        ))
+      }
+    )
+    .switchMap(
+      (() => {
+        return  fromPromise(firebase.auth().currentUser.getIdToken())
+      })
+    )
+    .mergeMap(
+      (token: string) => {
+        return [
+          {
+            type: AuthActions.SIGNIN
+          },
+          {
+            type: AuthActions.SET_TOKEN,
+            payload: token
+          } 
+        ]
+      }
+    )
+
+
   constructor(private actions$: Actions){}
 }
