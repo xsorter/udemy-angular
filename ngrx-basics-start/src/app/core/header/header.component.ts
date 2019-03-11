e49a36d5afde89c49cee0ef5fd3eeb0b751c22e6
Oleksii Nelin <xsorter@gmail.com>
2019-03-12 00:16:25 +0200

    logout action dispatched

--- ngrx-basics-start/src/app/core/header/header.component.ts
+++ ngrx-basics-start/src/app/core/header/header.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 // import { HttpEvent, HttpEventType } from '@angular/common/http';
 
 import { DataStorageService } from '../../shared/data-storage.service';
-import { AuthService } from '../../auth/auth.service';
 import { Store } from '@ngrx/store';
 import * as fromApp from '../../store/app.reducers';
 import * as fromAuth from '../../auth/store/auth.reducers';
 import { Observable } from 'rxjs';
+import * as AuthActions from '../../auth/store/auth.actions';
 
 @Component({
   selector: 'app-header',
@@ -14,7 +14,6 @@ import { Observable } from 'rxjs';
 })
 export class HeaderComponent implements OnInit{
   constructor(private dataStorageService: DataStorageService,
-              private authService: AuthService,
               private store: Store<fromApp.AppState>) {
   }
 
@@ -38,6 +37,6 @@ export class HeaderComponent implements OnInit{
   }
 
   onLogout() {
-    this.authService.logout();
+    this.store.dispatch(new AuthActions.Logout());
   }
 }
