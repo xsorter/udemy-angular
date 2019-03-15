28c6929eab2caa9f2c1e70d316f37e6dbd87fc3c
Oleksii Nelin <xsorter@gmail.com>
2019-03-16 00:01:51 +0200

    import router-store

--- ngrx-basics-start/src/app/app.module.ts
+++ ngrx-basics-start/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { StoreModule } from '@ngrx/store';
 import { reducers } from './store/app.reducers';
 import { EffectsModule } from '@ngrx/effects';
 import { AuthEffects } from './auth/store/auth.effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [
@@ -21,12 +22,13 @@ import { AuthEffects } from './auth/store/auth.effects';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    SharedModule,
+    SharedModule, 
     ShoppingListModule,
     AuthModule,
     CoreModule,
     StoreModule.forRoot(reducers),
-    EffectsModule.forRoot([AuthEffects])
+    EffectsModule.forRoot([AuthEffects]),
+    StoreRouterConnectingModule
   ],
   bootstrap: [AppComponent]
 })
