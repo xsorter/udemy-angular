b02494273c256a62f395d83f5a4503f3bef8cdbe
Oleksii Nelin <xsorter@gmail.com>
2019-04-17 23:57:55 +0300

    using keyframes for animations

--- animations-start/src/app/app.component.ts
+++ animations-start/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -52,6 +52,42 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
           'transform': 'translateX(-100px)'
         }),
         animate(400)
+      ]),
+      transition('* => void', [
+        animate(400, style({
+          'opacity':'0',
+          'transform': 'translateX(100px)'
+        }))
+      ])
+    ]),
+    trigger('list2', [
+      state('in', style({
+        'opacity':'1',
+        'transform': 'translateX(0)'
+      })),
+      transition('void => *', [
+        animate(1000, keyframes([
+          style({
+            'transform': 'translateX(-100px)',
+            'opacity': '0',
+            offset: 0
+          }),
+          style({
+            'transform': 'translateX(-50px)',
+            'opacity': '0.5',
+            offset: 0.3
+          }),
+          style({
+            'transform': 'translateX(-20px)',
+            'opacity': '1',
+            offset: 0.8
+          }),
+          style({
+            'transform': 'translateX(0px)',
+            'opacity': '1',
+            offset: 1
+          })
+        ]))
       ]),
       transition('* => void', [
         animate(400, style({
