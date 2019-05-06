b097a1d1f8e6034931297b2f207fb9965637cc29
Oleksii Nelin <xsorter@gmail.com>
2019-05-06 23:58:01 +0300

    simulating async tasks

--- unit-testing/src/app/user/user.component.spec.ts
+++ unit-testing/src/app/user/user.component.spec.ts
@@ -2,19 +2,20 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserComponent } from './user.component';
 import { UserService } from './user.service';
+import { DataService } from '../shared/data.service';
 
 describe('UserComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [UserComponent]
     });
-  })
+  });
 
   it('should create the app', () => {
     let fixture = TestBed.createComponent(UserComponent);
     let app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  })
+  });
 
   it('should use username from service', () => {
     let fixture = TestBed.createComponent(UserComponent);
@@ -22,7 +23,7 @@ describe('UserComponent', () => {
     let userService = fixture.debugElement.injector.get(UserService);
     fixture.detectChanges();
     expect(userService.user.name).toEqual(app.user.name);
-  })
+  });
 
   it('should display username if the user is logged in', () => {
     let fixture = TestBed.createComponent(UserComponent);
@@ -31,7 +32,7 @@ describe('UserComponent', () => {
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('p').textContent).toContain(app.user.name);
-  })
+  });
 
   it('shouldnot display username if the user is not logged in', () => {
     let fixture = TestBed.createComponent(UserComponent);
@@ -39,5 +40,28 @@ describe('UserComponent', () => {
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('p').textContent).not.toContain(app.user.name);
-  })
+  });
+
+  it('sholud not fetch data successfully if not async', () => {
+    let fixture = TestBed.createComponent(UserComponent);
+    let app = fixture.debugElement.componentInstance;
+    let dataService = fixture.debugElement.injector.get(DataService);
+    let spy = spyOn(dataService, 'getDetails')
+      .and.returnValue(Promise.resolve('Data'));
+    fixture.detectChanges();
+    expect(app.data).toBe(undefined);  
+  });
+
+  it('sholud fetch data successfully if async', async(() => {
+    let fixture = TestBed.createComponent(UserComponent);
+    let app = fixture.debugElement.componentInstance;
+    let dataService = fixture.debugElement.injector.get(DataService);
+    let spy = spyOn(dataService, 'getDetails')
+      .and.returnValue(Promise.resolve('Data'));
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(app.data).toBe(undefined);  
+    });
+  
+  }));
 });
